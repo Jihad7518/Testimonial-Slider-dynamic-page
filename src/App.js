c123b746eb60f90de9471332adcec0ae6f39e1b5
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-03 02:42:59 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 import React from "react";
 import Testimonial from "./components/Testimonial";
 import reviews from "./data";
+
+const App = () => {
+  return (
+    <div className="flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200">
+      <div className="text-center">
+        <h2 className="text-4xl font-bold mb-2">Our Testimonials</h2>
+        <div  className="bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+        <Testimonial reviews={reviews}/>
+      </div>
+    </div>
+  )
+};

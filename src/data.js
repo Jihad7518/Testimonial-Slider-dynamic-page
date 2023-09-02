4a0e4d6dac1566b2e3249a4ce460964617aff1ec
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-03 02:43:39 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,1 +1,15 @@
-
+const reviews = [
+  {
+    id: 1,
+    name: "Md. Jihad",
+    job: "Full-Stack Developer",
+    image: "https://avatars.githubusercontent.com/u/125210346?s=400&u=c7c47dc470ef93ecc22ac576324e4b7ecb6bd458&v=4",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "Lotifa Akan Anannya",
+    job: "System Analyst",
+    image: "https://scontent.fdac11-2.fna.fbcdn.net/v/t1.6435-9/193558172_937733826960778_661731698164894670_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_eui2=AeG1ALqorEpo3WOT8gqi4jRhaWGMo1jfdtVpYYyjWN921RFBNq_v4x33CuWOO0JejkSDwwkgufw8bP6i9Ivse3aY&_nc_ohc=L5w8LUUCTjUAX8PalpO&_nc_ht=scontent.fdac11-2.fna&oh=00_AfBSFCA0_HyxYPO0ZrCFCGaTU0vtz5THLfoJT0_7mRFmgQ&oe=651985C6",
+    text: "I have been working with them for several projects now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },

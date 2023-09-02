1042eb7cfea57892c6a8b6f053112abc995ca726
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-03 02:43:53 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -13,3 +13,17 @@ const reviews = [
     image: "https://scontent.fdac11-2.fna.fbcdn.net/v/t1.6435-9/193558172_937733826960778_661731698164894670_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_eui2=AeG1ALqorEpo3WOT8gqi4jRhaWGMo1jfdtVpYYyjWN921RFBNq_v4x33CuWOO0JejkSDwwkgufw8bP6i9Ivse3aY&_nc_ohc=L5w8LUUCTjUAX8PalpO&_nc_ht=scontent.fdac11-2.fna&oh=00_AfBSFCA0_HyxYPO0ZrCFCGaTU0vtz5THLfoJT0_7mRFmgQ&oe=651985C6",
     text: "I have been working with them for several projects now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
   },
+  {
+    id: 3,
+    name: "Ab. Rahim Ahmed",
+    job: "Project Manager",
+    image: "https://avatars.githubusercontent.com/u/89537538?v=4",
+    text: "I am thrilled with the results of our hard work, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Nur Adnan Chowdhury",
+    job: "Full-Stack Developer",
+    image: "https://scontent.fdac11-2.fna.fbcdn.net/v/t39.30808-6/369985451_3526592004265706_1439271548437439463_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a2f6c7&_nc_eui2=AeErlloayelXRqDk_zS1wdC99cpniU1Yqp_1ymeJTViqn_ZD5Rm2BHrmvcifn8GmVNWc3f7A6D2V_aFm0mEk1oR5&_nc_ohc=Dvkn46ieTpQAX8Cl54P&_nc_ht=scontent.fdac11-2.fna&oh=00_AfDhw6EZOlj1VEroGIgDdgf4TGBbZ3bisBKoUF_ktfY0sg&oe=64F73EB3",
+    text: "I have worked with many developers over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },

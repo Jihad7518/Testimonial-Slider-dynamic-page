4343d151b7c5df4404d8fd31498440ab4011fa9e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-06 21:04:02 +0600

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,3 +14,18 @@ const Card = (props) => {
             <div className='w-[170px] h-[170px] bg-violet-500 rounded-full absolute
                 top-[-6px] z-[-10] left-[10px]'>
             </div>
+</div>
+
+        <div className='text-center mt-7'>
+        <p className='tracking-wider font-bold text-2xl capitalize'>{review.name}</p>
+        <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
+      </div>
+
+
+        <div className='text-violet-400 mx-auto mt-5'>
+            <FaQuoteLeft/>
+        </div>
+
+        <div className='text-center mt-4 text-slate-500'>
+            {review.text}
+        </div>

9e71a2457d29023aaf8b32e59ff1afac04fc0a01
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-07 03:16:30 +0600

    Update data.js

--- src/data.js
+++ src/data.js
@@ -10,7 +10,7 @@ const reviews = [
     id: 2,
     name: "Lotifa Akan Anannya",
     job: "System Analyst",
-    image: "https://scontent.fdac11-2.fna.fbcdn.net/v/t1.6435-9/193558172_937733826960778_661731698164894670_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=174925&_nc_eui2=AeG1ALqorEpo3WOT8gqi4jRhaWGMo1jfdtVpYYyjWN921RFBNq_v4x33CuWOO0JejkSDwwkgufw8bP6i9Ivse3aY&_nc_ohc=L5w8LUUCTjUAX8PalpO&_nc_ht=scontent.fdac11-2.fna&oh=00_AfBSFCA0_HyxYPO0ZrCFCGaTU0vtz5THLfoJT0_7mRFmgQ&oe=651985C6",
+    image: "https://avatars.githubusercontent.com/u/129364460?v=4",
     text: "I have been working with them for several projects now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
   },
   {

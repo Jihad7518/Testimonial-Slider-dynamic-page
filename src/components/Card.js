fc067d8aa84cba9897f677fd5f3382bec72169fc
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-06 21:03:38 +0600

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,1 +1,16 @@
 
+import React from 'react'
+import {FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
+
+const Card = (props) => {
+    let review = props.review;
+
+  return (
+    <div className='flex flex-col md:relative'>
+        <div className='absolute top-[-7rem] z-[10] mx-auto'>
+            <img className="aspect-square rounded-full w-[170px] h-[170px] z-[25]"
+             src={review.image}></img>
+
+            <div className='w-[170px] h-[170px] bg-violet-500 rounded-full absolute
+                top-[-6px] z-[-10] left-[10px]'>
+            </div>

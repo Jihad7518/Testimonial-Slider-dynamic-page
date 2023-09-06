e872f9cf3d89309d3b04618a7fdbf8b5194777c0
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-06 21:05:06 +0600

    Update Testimonial.js

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -8,3 +8,20 @@ import { useState } from 'react';
 const Testimonials = (props) => {
     let reviews = props.reviews;
     const [index, setIndex] = useState(0);
+    
+    function leftShiftHandler() {
+        if(index - 1 < 0) {
+            setIndex(reviews.length - 1);
+        }
+        else {
+            setIndex(index - 1);
+        }
+    }
+
+    function rightShiftHandler() {
+        if(index + 1 >=reviews.length) {
+            setIndex(0);
+        }
+        else {
+            setIndex(index+1);
+        }

56bdda602a7fb1d82752e6fd548ce907f737efec
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-09-06 21:04:50 +0600

    Update Testimonial.js

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -1,1 +1,10 @@
 
+import React from 'react'
+import Card from './Card';
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { useState } from 'react';
+
+
+const Testimonials = (props) => {
+    let reviews = props.reviews;
+    const [index, setIndex] = useState(0);
